65e6e553413a13e9439dfd5815de83f90bbfdeaf
unknown <pappas.g.alexandros@gmail.com>
2022-02-04 11:28:29 +0200

    Adding the menu dynamically

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
 
-// searchBar.addEventListener('keyup', (e)=>{
-//     console.log( e.target.value);
-//     function searchBarShow(title){
-//         const link = `https://elearning-aueb.herokuapp.com/courses/search?title=${title}`;
-//         fetch(link)
-//         .then(response => response.json)
-//         .then((data)=>{
-//             console.log(data);
-//         })
-//     }
-
-// fetch(`https://elearning-aueb.herokuapp.com/courses/search?title=1`)
-// .then(response=> response.json())
-// .then(data => console.log(data));
 // fetch(`https://elearning-aueb.herokuapp.com/courses/search?category=id : `)
 // .then(response=> response.json())
 // .then(data => console.log(data));
@@ -27,18 +13,37 @@ fetch(`https://elearning-aueb.herokuapp.com/categories`)
     console.log(categories)
     for(let i = 0; i < categories.length; i++){
         console.log(categories[i].title);
-        document.querySelector('.categories').innerHTML = `<li>${categories[i].title}</li>`;
+        //To valame apla gia elegxo
     }
-    return data;
+    const [a, b, c, d, e] = data
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    console.log(d)
+    console.log(e)
+    const newdata = [...data];
+    // console.log(newdata)
+    console.log(...newdata);
+    // const [id1, ...title1] = [...newdata]
+    // console.log(id1)
+    // const {id2, title2} = id1
+    // console.log(id2)
+    const {id, title} = a
+    console.log(id, title)
 });
 
-
+// Search var
 function searchBarShow(keyword){
     const link = `https://elearning-aueb.herokuapp.com/courses/search?title=${keyword}`;
     fetch(link)
-    .then(response => response.json)
+    .then(response => response.json())
     .then((jsonData)=>{
         console.log(jsonData);
+        let template = Handlebars.compile(document.querySelector('#template').innerHTML);
+        let filled = template(jsonData, {
+            noEscape: true
+        });
+        document.querySelector('.courses').innerHTML = filled;
     })
 }
 
@@ -47,20 +52,32 @@ window.onload = () =>{
     const searchBar = document.getElementById('search-txt');
     searchBar.onkeyup = ()=>{
        searchBarShow(searchBar.value);
-       console.log(searchBar.value)
+
     }
 }
+//Course Navigation 
+function fill_template(){
+    fetch(`https://elearning-aueb.herokuapp.com/categories`)
+    .then(response => response.json())
+    .then(data => {
+        let template = Handlebars.compile(document.querySelector('#templatenav').innerHTML);
+        let filled = template(data, {
+            noEscape: true
+        });
+        document.querySelector('.dropdown-menu').innerHTML = filled;
+});
+}
+//Display courses by id
 
-// function fill_template(){
-//     let data = {
-//         title: 'alekos',
-//         list : [
-//             {name:'you can generate stuff'},
-//             {name:'you can generate stuff2'},
-//             {name:'you can generate stuff3'},
-//         ]
-//     };
-//     let template = Handlebars.compile(document.querySelector('#template').innerHTML);
-//     let filled = template(data);
-//     document.querySelector('#output').innerHTML = filled;
-// }
+function display_by_id(id){
+    fetch(`https://elearning-aueb.herokuapp.com/courses/search?category=${id}`)
+    .then(response=> response.json())
+    .then(data => {
+        console.log(data);
+        let template = Handlebars.compile(document.querySelector('courses-template-id').innerHTML);
+        let filled = template(jsonData, {
+            noEscape: true
+        });
+        document.querySelector('.all-courses').innerHTML = filled;
+    });
+}
